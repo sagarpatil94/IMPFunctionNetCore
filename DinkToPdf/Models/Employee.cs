59938326a3be2f60e2530d13220c7aca9e82f304
nhanpt109 <nhanpt@gemvietnam.com>
2021-01-29 23:44:34 +0700

    microsoft team project

---
+++ DinkToPdf/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinkToPdf.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string Add { get; set; }
+    }
+}
---
+++ DinkToPdf/Repositories/EmployeeRepository.cs
@@ -0,0 +1,108 @@
+﻿using DinkToPdf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinkToPdf.Repositories
+{
+    public class EmployeeRepository
+    {
+        public static List<Employee> EmployeesData()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    Name = "Mike",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 2,
+                    Name = "Mike1",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 3,
+                    Name = "Mike2",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 4,
+                    Name = "Mike3",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 5,
+                    Name = "Mike4",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 6,
+                    Name = "Mike5",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 7,
+                    Name = "Mike6",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 8,
+                    Name = "Mike7",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 9,
+                    Name = "Mike8",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 10,
+                    Name = "Mike9",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+                new Employee()
+                {
+                    Id = 11,
+                    Name = "Mike10",
+                    Add = "Chicago",
+                    Age = 10,
+                    PhoneNumber = "08444444444444"
+                },
+
+            };
+            return employees;
+        }
+    }
+}
