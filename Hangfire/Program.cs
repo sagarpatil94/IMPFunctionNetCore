a465e8a90a35578ae4eafba1ec2322fe481f3f71
nhan1110i <trongnhan1110i@gmail.com>
2021-08-21 21:21:28 +0700

    Renew solution.

---
+++ Hangfire/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangfire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
